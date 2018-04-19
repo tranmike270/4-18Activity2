b09e2a5dabd82f51b30048d76b6d31cfc5069c99
Michael Tran <tranmike196@gmail.com>
2018-04-18 19:39:28 -0500

    Added Bonus

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var PORTB = 7500;
 // Functions to handle the different request
 function handleRequest(request, response){
     
-    response.end("You're amazing!\n");
+    response.end(stuff("Good"));
 };
 function handleRequestB(request, response){
 
-    response.end("You're terrible!");
+    response.end(stuff("Bad"));
 };
 
 
@@ -27,3 +27,15 @@ var server2 = http.createServer(handleRequestB);
 server2.listen(PORTB, function(){
     console.log("Listening to PORT: " + PORTB);
 });
+
+
+function stuff(arrr){
+    var arr = ["You're nice","You're amazing","You're the best"];
+    var arrB = ["You're terrible", "You're horrific", "You're the worst"];
+    var rand = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    if(arrr === "Good"){
+        return arr[rand];
+    }else {
+        return arrB[rand];
+    };
+};
